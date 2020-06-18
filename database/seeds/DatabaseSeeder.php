53f0e1e85ef771fd5b1cb49d5684456114bef064
javicuc-ryrx <javier.lopez@alumnos.udg.mx>
2020-06-18 13:18:43 -0500

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
             Item::create([
                 'name'=> $faker->catchPhrase(),
                 'description'=> $faker->sentence(6,true),
-                'qrcode' => 'https://chart.googleapis.com/chart?cht=qr&chs=150&chl=http://itemsmsapi.test:8080/items/'.$i.'&choe=UTF-8',
+                'qrcode' => 'https://chart.googleapis.com/chart?cht=qr&chs=150&chl='.$i.'&choe=UTF-8',
                 'imgItem' => url('/') . '/itemsphotos/itemdefault.png',
                 'laboratory_id' => $faker->numberBetween(1,4)
             ]);
